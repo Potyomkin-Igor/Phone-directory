b36d47412b02565faee7c265547d353323483e53
Igor Potyomkin <Original2409@gmail.com>
2017-07-11 03:21:51 +0300

    feat: Added logback.xml fix: a bug fix in import.sql
    Deleted ValidationMessages.properties

--- src/main/java/net/keepers/Main.java
+++ src/main/java/net/keepers/Main.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
 
-    public static void main(String[] args   ) {
+    public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 --   ----------------------------------------------------------------------------------------------
 -- Fill contacts table
 --   ----------------------------------------------------------------------------------------------
-INSERT INTO phone_directory.contacts (id, phone, type, comment) VALUES(1, '+380687321547','MOBILE','Mobile contact number'), (2, '+38(068)8965214','MOBILE','It is my contact number'),(3, '+380680002578','MOBILE','It is my contact number'),(4, '4042536','HOME','It is my contact home number'),(5, '+380683210000','','It is my contact number'),(6, '71-11-89','HOME','It is my contact number'),(7, '+380683210000','','It is my contact number'),(8, '+380683219999','MOBILE','It is my contact number'), (9, '(068)3210858','','It is my contact number'),(10, '+38(068)5053114','SOME_TO CHANGE','It is my contact number');
+INSERT INTO phone_directory.contacts (id, phone, type, comment) VALUES(1, '+380687321547','MOBILE','Mobile contact number'), (2, '+380688965214','MOBILE','It is my contact number'),(3, '+380680002578','MOBILE','It is my contact number'),(4, '4042536','HOME','It is my contact home number'),(5, '+380683210000','','It is my contact number'),(6, '71-11-89','HOME','It is my contact number'),(7, '+380683210000','','It is my contact number'),(8, '+380683219999','MOBILE','It is my contact number'), (9, '(068)3210858','','It is my contact number'),(10, '+380685053114','SOME_TO CHANGE','It is my contact number');
 
 --   ----------------------------------------------------------------------------------------------
 -- Fill users table
